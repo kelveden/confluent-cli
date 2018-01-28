892ccfef8e627e7e3c68ecdfa38eba116087ab82
Alistair Dutton <kelveden@gmail.com>
2018-01-28 11:35:15 +0000

    Make use of profiles.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,6 @@
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
-    mocha = require('gulp-mocha'),
-    jshintStylish = require('jshint-stylish'),
-    complexity = require('gulp-complexity'),
-    rimraf = require('rimraf');
-
-gulp.task('complexity', function () {
-    return gulp.src('lib/**/*.js')
-        .pipe(complexity({
-            cyclomatic: [ 5 ],
-            halstead: [ 16 ],
-            maintainability: [ 100 ]
-        }));
-});
+    jshintStylish = require('jshint-stylish');
 
 gulp.task('lint', function () {
     gulp.src([ 'gulpfile.js', 'lib/**/*.js' ])
@@ -21,15 +9,4 @@ gulp.task('lint', function () {
         .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('test', function () {
-    return gulp.src('test/**/*')
-        .pipe(mocha());
-});
-
-gulp.task('compile', function () {
-    gulp.src('lib/**/*.json')
-        .pipe(gulp.dest('./dist'));
-});
-
-
-gulp.task('default', [ 'lint', 'test', 'compile' ]);
+gulp.task('default', [ 'lint' ]);
